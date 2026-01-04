72ba4059e4bc68496386403879b0637e695aa2df
Mario Lungu <mario.lungu@studio.unibo.it>
2026-01-04 23:17:00 +0100

    init MenuActionsInterface

--- src/main/java/it/unibo/scat/view/api/MenuActionsInterface.java
+++ src/main/java/it/unibo/scat/view/api/MenuActionsInterface.java
@@ -1,8 +1,64 @@
 package it.unibo.scat.view.api;
 
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import it.unibo.scat.common.EntityView;
+import it.unibo.scat.model.leaderboard.Leaderboard;
+
 /**
  * Interface for the View class, used by MenuPanel, GamePanel.
  */
 public interface MenuActionsInterface {
 
+    /**
+     * @return ...
+     * 
+     */
+    JFrame getFrame();
+
+    /**
+     * ...
+     */
+    void pauseGame();
+
+    /**
+     * ...
+     */
+    void resumeGame();
+
+    /**
+     * ...
+     */
+    void resetGame();
+
+    /**
+     * ...
+     */
+    void quitGame();
+
+    /**
+     * @param username ...
+     * 
+     */
+    void setUsername(String username);
+
+    /**
+     * @return ...
+     * 
+     */
+    List<EntityView> fetchEntitiesFromModel();
+
+    /**
+     * @return ...
+     * 
+     */
+    Leaderboard fetchLeaderboard();
+
+    /**
+     * @return ...
+     * 
+     */
+    String fetchUsername();
 }
