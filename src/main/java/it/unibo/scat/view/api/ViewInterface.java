4f8ad66abb0b49cc459ca031c38102b3fa3b3649
Girolamo Ronzoni <girolamo.ronzoni@studio.unibo.it>
2026-01-05 01:30:17 +0100

    init ViewInterface

--- src/main/java/it/unibo/scat/view/api/ViewInterface.java
+++ src/main/java/it/unibo/scat/view/api/ViewInterface.java
@@ -4,5 +4,13 @@ package it.unibo.scat.view.api;
  * Interface for the View class, used by Control.
  */
 public interface ViewInterface {
+    /**
+     * ...
+     */
+    void closeFrame();
 
+    /**
+     * ...
+     */
+    void initEverything();
 }
