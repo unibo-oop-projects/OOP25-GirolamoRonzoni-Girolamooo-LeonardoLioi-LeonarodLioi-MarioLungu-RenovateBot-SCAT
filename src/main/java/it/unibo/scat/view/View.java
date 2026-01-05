1d96dfd3c6dddc293a3b71d1e38b7c01a9f8a3f5
Girolamo Ronzoni <girolamo.ronzoni@studio.unibo.it>
2026-01-05 16:42:44 +0100

    added fields in View.java

--- src/main/java/it/unibo/scat/view/View.java
+++ src/main/java/it/unibo/scat/view/View.java
@@ -1,8 +1,22 @@
 package it.unibo.scat.view;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import it.unibo.scat.control.api.ControlInterface;
+import it.unibo.scat.view.game.GamePanel;
+import it.unibo.scat.view.menu.MenuPanel;
+
 /**
  * The main class for the "View" section of the MVC pattern.
  */
+@SuppressFBWarnings("UUF_UNUSED_FIELD")
 public class View {
-
+    private ControlInterface controlInterface;
+    private JFrame frame;
+    private Dimension screenDim;
+    private MenuPanel menuPanel;
+    private GamePanel gamePanel;
 }
