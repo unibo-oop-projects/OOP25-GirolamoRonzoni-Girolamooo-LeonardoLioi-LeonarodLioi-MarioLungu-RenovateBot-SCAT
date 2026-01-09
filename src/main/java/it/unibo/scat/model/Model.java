657fd508fcc31fc70ffc7a3f1b28c543b54ff5bc
girolamooo <pocheparolee@gmail.com>
2026-01-09 16:37:40 +0100

    updated fields in Model

--- src/main/java/it/unibo/scat/model/Model.java
+++ src/main/java/it/unibo/scat/model/Model.java
@@ -1,13 +1,9 @@
 package it.unibo.scat.model;
 
-import java.util.List;
-
-import javax.swing.text.html.parser.Entity;
-
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.scat.model.game.entity.Invader;
-import it.unibo.scat.model.game.entity.Player;
-import it.unibo.scat.model.game.entity.Shot;
+import it.unibo.scat.common.GameState;
+import it.unibo.scat.model.game.GameLogic;
+import it.unibo.scat.model.game.GameWorld;
 import it.unibo.scat.model.leaderboard.Leaderboard;
 
 /**
@@ -15,16 +11,13 @@ import it.unibo.scat.model.leaderboard.Leaderboard;
  */
 @SuppressFBWarnings(value = "UUF_UNUSED_FIELD", justification = "Fields will be used by upcoming game logic")
 public class Model {
-
-    private List<Entity> entities;
-    private Player player;
-    private List<Invader> invaders;
-    private List<Shot> shots;
     private int score;
-    private Leaderboard leaderboard;
-    private int gameState;
     private int level;
     private String username;
+    private GameState gameState;
+    private Leaderboard leaderboard;
+    private GameWorld gameWorld;
+    private GameLogic gameLogic;
 
     /**
      * ...
@@ -32,4 +25,5 @@ public class Model {
     void increaseLevel() {
 
     }
+
 }
