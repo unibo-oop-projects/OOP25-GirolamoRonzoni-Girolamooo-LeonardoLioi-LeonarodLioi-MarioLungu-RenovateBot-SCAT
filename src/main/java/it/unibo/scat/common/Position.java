ee11adb5be8fda86fa39f83704e181413396baeb
girolamooo <pocheparolee@gmail.com>
2026-01-10 21:53:47 +0100

    wrote Position class with his constructor and methods

--- src/main/java/it/unibo/scat/common/Position.java
+++ src/main/java/it/unibo/scat/common/Position.java
@@ -4,5 +4,49 @@ package it.unibo.scat.common;
  * This class is used to manage the position of an entity.
  */
 public class Position {
+    private int x;
+    private int y;
+
+    /**
+     * @param x ...
+     * @param y ...
+     * 
+     */
+    public Position(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return ...
+     * 
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return ...
+     * 
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param x ...
+     * 
+     */
+    public void setX(final int x) {
+        this.x = x;
+    }
+
+    /**
+     * @param y ...
+     * 
+     */
+    public void setY(final int y) {
+        this.y = y;
+    }
 
 }
