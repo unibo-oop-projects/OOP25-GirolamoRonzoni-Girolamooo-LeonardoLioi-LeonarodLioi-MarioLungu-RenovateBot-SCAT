15a2af8eed2593e4ddd147e9e4a781cd55901ee3
girolamooo <pocheparolee@gmail.com>
2026-01-10 22:43:10 +0100

    Added functions signature in Leaderboard

--- src/main/java/it/unibo/scat/model/leaderboard/Leaderboard.java
+++ src/main/java/it/unibo/scat/model/leaderboard/Leaderboard.java
@@ -1,5 +1,6 @@
 package it.unibo.scat.model.leaderboard;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -12,9 +13,40 @@ public class Leaderboard {
     private List<Record> games;
 
     /**
-     * ...
+     * Costruttore vuoto di default.
      */
     public Leaderboard() {
+        // Default constructor
+    }
 
+    /**
+     * @param filename ...
+     * 
+     */
+    public void initLeaderboard(final String filename) {
+
+    }
+
+    /**
+     * ...
+     */
+    public void updateFile() {
+
+    }
+
+    /**
+     * @param newRecord ...
+     * 
+     */
+    public void addNewRecord(final Record newRecord) {
+
+    }
+
+    /**
+     * @return ...
+     * 
+     */
+    public List<Record> getAllRecords() {
+        return new ArrayList<>();
     }
 }
