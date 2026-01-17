5a7aa69bb4acbd3c52bfbcbee494b0c0ffd4315d
Mario Lungu <mario.lungu@studio.unibo.it>
2026-01-17 22:44:07 +0100

    MenuPanel: added background

--- src/main/java/it/unibo/scat/view/menu/MenuPanel.java
+++ src/main/java/it/unibo/scat/view/menu/MenuPanel.java
@@ -1,5 +1,11 @@
 package it.unibo.scat.view.menu;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
@@ -7,11 +13,43 @@ import javax.swing.JPanel;
  */
 public class MenuPanel extends JPanel {
     private static final long serialVersionUID = 1L;
+    private BufferedImage background;
 
     /**
      * ...
      */
     public MenuPanel() {
-        // wait
+        try {
+            background = ImageIO.read(
+                    Objects.requireNonNull(
+                            getClass().getResource("/images/menu_background.jpg")));
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot load menu background", e);
+        }
+
     }
+
+    @Override
+    protected void paintComponent(final Graphics g) {
+        super.paintComponent(g);
+
+        int panelW = getWidth();
+        int panelH = getHeight();
+
+        int imgW = background.getWidth();
+        int imgH = background.getHeight();
+
+        double scale = Math.max(
+                (double) panelW / imgW,
+                (double) panelH / imgH);
+
+        int drawW = (int) Math.ceil(imgW * scale);
+        int drawH = (int) Math.ceil(imgH * scale);
+
+        int x = (panelW - drawW) / 2;
+        int y = (panelH - drawH) / 2;
+
+        g.drawImage(background, x, y, drawW, drawH, null);
+    }
+
 }
