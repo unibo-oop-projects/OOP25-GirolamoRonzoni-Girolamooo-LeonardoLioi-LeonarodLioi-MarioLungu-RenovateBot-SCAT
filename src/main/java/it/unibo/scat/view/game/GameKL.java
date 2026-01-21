a437630e54a4a76a2f891e384fe9cd22c41ae912
Mario Lungu <mario.lungu@studio.unibo.it>
2026-01-21 13:49:13 +0100

    GameKl: wrote "keyPressed()" and added other KeyListener methods

--- src/main/java/it/unibo/scat/view/game/GameKL.java
+++ src/main/java/it/unibo/scat/view/game/GameKL.java
@@ -1,12 +1,73 @@
 package it.unibo.scat.view.game;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import it.unibo.scat.common.Direction;
 import it.unibo.scat.control.api.ControlInterface;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Custom KeyListener for the GamePanel.
  */
-@SuppressFBWarnings("UUF_UNUSED_FIELD")
-public class GameKL {
-    private ControlInterface controlInterface;
+public class GameKL implements KeyListener {
+    private final ControlInterface controlInterface;
+
+    /**
+     * need it to pass the tests.
+     * 
+     * @param controlInterface ..
+     */
+    public GameKL(final ControlInterface controlInterface) {
+        this.controlInterface = controlInterface;
+    }
+
+    /**
+     * @param e ...
+     * 
+     */
+    @Override
+    public void keyTyped(final KeyEvent e) {
+
+    }
+
+    /**
+     * @param e ...
+     * 
+     */
+    @Override
+    public void keyPressed(final KeyEvent e) {
+        final int eventCode = e.getKeyCode();
+
+        switch (eventCode) {
+            case KeyEvent.VK_SPACE:
+                controlInterface.notifyPlayerShot();
+                break;
+
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_LEFT:
+                controlInterface.notifyPlayerMovement(Direction.LEFT);
+                break;
+
+            case KeyEvent.VK_D:
+            case KeyEvent.VK_RIGHT:
+                controlInterface.notifyPlayerMovement(Direction.RIGHT);
+                break;
+
+            case KeyEvent.VK_ESCAPE:
+                controlInterface.notifyPauseGame();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * @param e ...
+     * 
+     */
+    @Override
+    public void keyReleased(final KeyEvent e) {
+
+    }
 }
