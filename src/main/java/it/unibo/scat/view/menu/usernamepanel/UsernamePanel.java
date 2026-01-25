745e145d92779b002c09cf9777c1e78ae35fdd94
Mario Lungu <mario.lungu@studio.unibo.it>
2026-01-25 22:27:30 +0100

    UsernamePanel: init

--- src/main/java/it/unibo/scat/view/menu/usernamepanel/UsernamePanel.java
+++ src/main/java/it/unibo/scat/view/menu/usernamepanel/UsernamePanel.java
@@ -1,6 +1,11 @@
 package it.unibo.scat.view.menu.usernamepanel;
 
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import it.unibo.scat.view.api.MenuActionsInterface;
 
@@ -10,9 +15,29 @@ import it.unibo.scat.view.api.MenuActionsInterface;
 public final class UsernamePanel extends JPanel {
     private static final long serialVersionUID = 1L;
     private final MenuActionsInterface menuActionsInterface;
+    private JTextField usernameField;
+    private final Font baseFont = new Font("Calibri", Font.BOLD, 80);
     private String username = "user";
 
+    /**
+     * @param menuActionsInterface ...
+     * 
+     */
     public UsernamePanel(final MenuActionsInterface menuActionsInterface) {
         this.menuActionsInterface = menuActionsInterface;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        initUsernameField();
+    }
+
+    /**
+     * ...
+     */
+    private void initUsernameField() {
+
+        usernameField = new JTextField("user");
+
+        add(Box.createVerticalGlue());
+        add(usernameField);
     }
 }
