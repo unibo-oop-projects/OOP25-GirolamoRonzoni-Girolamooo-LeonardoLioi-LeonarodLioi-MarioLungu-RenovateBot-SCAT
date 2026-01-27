65359cc4268e51c45979241954c63df8f8e647ba
girolamooo <pocheparolee@gmail.com>
2026-01-27 15:30:06 +0100

    UIConstats: added constants for background images paths

--- src/main/java/it/unibo/scat/common/UIConstants.java
+++ src/main/java/it/unibo/scat/common/UIConstants.java
@@ -12,70 +12,76 @@ import java.util.List;
  * ...
  */
 public final class UIConstants {
-    // IMAGE PATHS
-    public static final String PLAYER_SHOT_PATH = "/entities/shots/player_shot.png";
-    public static final String INVADER_SHOT_PATH = "/entities/shots/player_shot.png";
-    public static final List<String> PLAYER_PATHS = List.of(
-            "/entities/player/ship1.png",
-            "/entities/player/ship2.png",
-            "/entities/player/ship3.png",
-            "/entities/player/ship4.png",
-            "/entities/player/ship5.png",
-            "/entities/player/ship6.png");
-    public static final List<String> BUNKER_PATHS = List.of(
-            "/entities/bunkers/bunker1.png",
-            "/entities/bunkers/bunker2.png",
-            "/entities/bunkers/bunker3.png");
-    public static final List<String> INVADER1_PATHS = List.of(
-            "/entities/invaders/invader_1_1.png",
-            "/entities/invaders/invader_1_2.png");
-    public static final List<String> INVADER2_PATHS = List.of(
-            "/entities/invaders/invader_2_1.png",
-            "/entities/invaders/invader_2_2.png");
-    public static final List<String> INVADER3_PATHS = List.of(
-            "/entities/invaders/invader_3_1.png",
-            "/entities/invaders/invader_3_2.png");
-    public static final List<String> BONUS_INVADER_PATHS = List.of(
-            "/entities/invaders/invader_4_1.png",
-            "/entities/invaders/invader_4_2.png");
-    public static final String NULL_PATH = "/entities/null.png";
+        // IMAGE PATHS
+        public static final String PLAYER_SHOT_PATH = "/entities/shots/player_shot.png";
+        public static final String INVADER_SHOT_PATH = "/entities/shots/player_shot.png";
+        public static final String MENU_BACKGROUND_PATH = "/backgrounds/menu_background.png";
+        public static final List<String> GAME_BACKGROUNDS_PATHS = List.of(
+                        "/backgrounds/game_background1.jpg",
+                        "/backgrounds/game_background2.jpg",
+                        "/backgrounds/game_background3.jpg",
+                        "/backgrounds/game_background4.jpg");
+        public static final List<String> PLAYER_PATHS = List.of(
+                        "/entities/player/ship1.png",
+                        "/entities/player/ship2.png",
+                        "/entities/player/ship3.png",
+                        "/entities/player/ship4.png",
+                        "/entities/player/ship5.png",
+                        "/entities/player/ship6.png");
+        public static final List<String> BUNKER_PATHS = List.of(
+                        "/entities/bunkers/bunker1.png",
+                        "/entities/bunkers/bunker2.png",
+                        "/entities/bunkers/bunker3.png");
+        public static final List<String> INVADER1_PATHS = List.of(
+                        "/entities/invaders/invader_1_1.png",
+                        "/entities/invaders/invader_1_2.png");
+        public static final List<String> INVADER2_PATHS = List.of(
+                        "/entities/invaders/invader_2_1.png",
+                        "/entities/invaders/invader_2_2.png");
+        public static final List<String> INVADER3_PATHS = List.of(
+                        "/entities/invaders/invader_3_1.png",
+                        "/entities/invaders/invader_3_2.png");
+        public static final List<String> BONUS_INVADER_PATHS = List.of(
+                        "/entities/invaders/invader_4_1.png",
+                        "/entities/invaders/invader_4_2.png");
+        public static final String NULL_PATH = "/entities/null.png";
 
-    // COLORS
-    public static final Color PANELS_BG_COLOR = new Color(255, 255, 255, 10);
+        // COLORS
+        public static final Color PANELS_BG_COLOR = new Color(255, 255, 255, 10);
 
-    // FONTS
-    public static final Font TITLE_FONT;
-    public static final Font TITLE_FONT_HOVER;
-    public static final Font MEDIUM_FONT;
-    public static final Font SMALL_FONT;
+        // FONTS
+        public static final Font TITLE_FONT;
+        public static final Font TITLE_FONT_HOVER;
+        public static final Font MEDIUM_FONT;
+        public static final Font SMALL_FONT;
 
-    static {
-        final float titleBase = 60f;
-        final float titleHover = 65f;
-        final float medium = 32f;
-        final float small = 20f;
+        static {
+                final float titleBase = 60f;
+                final float titleHover = 65f;
+                final float medium = 32f;
+                final float small = 20f;
 
-        try (InputStream is = UIConstants.class.getResourceAsStream("/fonts/PressStart2P.ttf")) {
+                try (InputStream is = UIConstants.class.getResourceAsStream("/fonts/PressStart2P.ttf")) {
 
-            final Font base = Font.createFont(Font.TRUETYPE_FONT, is);
-            GraphicsEnvironment
-                    .getLocalGraphicsEnvironment()
-                    .registerFont(base);
+                        final Font base = Font.createFont(Font.TRUETYPE_FONT, is);
+                        GraphicsEnvironment
+                                        .getLocalGraphicsEnvironment()
+                                        .registerFont(base);
 
-            TITLE_FONT = base.deriveFont(titleBase);
-            TITLE_FONT_HOVER = base.deriveFont(titleHover);
-            MEDIUM_FONT = base.deriveFont(medium);
-            SMALL_FONT = base.deriveFont(small);
+                        TITLE_FONT = base.deriveFont(titleBase);
+                        TITLE_FONT_HOVER = base.deriveFont(titleHover);
+                        MEDIUM_FONT = base.deriveFont(medium);
+                        SMALL_FONT = base.deriveFont(small);
 
-        } catch (final IOException | FontFormatException e) {
-            throw new ExceptionInInitializerError(e);
+                } catch (final IOException | FontFormatException e) {
+                        throw new ExceptionInInitializerError(e);
+                }
         }
-    }
 
-    /**
-     * Private constructor to avoid initialization.
-     */
-    private UIConstants() {
-    }
+        /**
+         * Private constructor to avoid initialization.
+         */
+        private UIConstants() {
+        }
 
 }
