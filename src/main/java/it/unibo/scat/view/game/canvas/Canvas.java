cfe5ced7173d832ac1f571d25817b3d9066db0ef
girolamooo <pocheparolee@gmail.com>
2026-02-03 09:06:57 +0100

    Canvas: introduced SpriteManager

--- src/main/java/it/unibo/scat/view/game/canvas/Canvas.java
+++ src/main/java/it/unibo/scat/view/game/canvas/Canvas.java
@@ -3,10 +3,8 @@ package it.unibo.scat.view.game.canvas;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -28,18 +26,12 @@ public final class Canvas extends JPanel {
     private final transient MenuActionsInterface menuActionsInterface;
     private transient volatile List<EntityView> entities;
     private transient Image voidImage;
-    private final transient Image[] player;
-    private final transient Image[] invader1;
-    private final transient Image[] invader2;
-    private final transient Image[] invader3;
-    private final transient Image[] invader4;
-    private final transient Image[] bunker;
-    private final transient Image[] playerShots;
-    private transient Image invaderShot;
     private final AtomicInteger invadersAnimationFrame = new AtomicInteger(0);
     private final AtomicInteger bonusInvaderAnimationFrame = new AtomicInteger(0);
     private int lastInvadersHash;
     private int lastBonusHash;
+
+    private transient SpriteManager spriteManger;
 
     /**
      * ...
@@ -48,20 +40,11 @@ public final class Canvas extends JPanel {
      */
     public Canvas(final MenuActionsInterface menuActionsInterface) {
         this.menuActionsInterface = menuActionsInterface;
-
-        invader1 = new Image[UIConstants.INVADER1_PATHS.size()];
-        invader2 = new Image[UIConstants.INVADER2_PATHS.size()];
-        invader3 = new Image[UIConstants.INVADER3_PATHS.size()];
-        invader4 = new Image[UIConstants.BONUS_INVADER_PATHS.size()];
-        bunker = new Image[UIConstants.BUNKER_PATHS.size()];
-        playerShots = new Image[UIConstants.PLAYER_SHOT_PATHS.size()];
-        player = new Image[UIConstants.PLAYER_PATHS.size()];
         entities = null; // to do for the checkstyle
 
         setForeground(UIConstants.WHITE_50_OPACITY);
         setFont(UIConstants.SMALL_FONT);
 
-        initImages();
         update();
     }
 
@@ -95,7 +78,6 @@ public final class Canvas extends JPanel {
      * @return a hash of the group position, or 0 if there are no entities of the
      *         given type
      */
-
     private static int hashPositions(final List<EntityView> entityList, final EntityType... types) {
         final int hashingValue = 31;
         int minX = Constants.BORDER_RIGHT;
@@ -123,67 +105,16 @@ public final class Canvas extends JPanel {
         return hash;
     }
 
-    /**
-     * ...
-     */
-    private void initImages() {
-        // VOID
-        voidImage = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.NULL_PATH))).getImage();
-
-        // PLAYER
-        player[0] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.PLAYER_PATHS.get(0)))).getImage();
-        player[1] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.PLAYER_PATHS.get(1)))).getImage();
-        player[2] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.PLAYER_PATHS.get(2)))).getImage();
-
-        // SHOTS
-        playerShots[0] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.PLAYER_SHOT_PATHS.get(0)))).getImage();
-        playerShots[1] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.PLAYER_SHOT_PATHS.get(1)))).getImage();
-        playerShots[2] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.PLAYER_SHOT_PATHS.get(2)))).getImage();
-        invaderShot = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.INVADER_SHOT_PATH))).getImage();
-
-        // BUNKERS
-        bunker[0] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.BUNKER_PATHS.get(0)))).getImage();
-        bunker[1] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.BUNKER_PATHS.get(1)))).getImage();
-        bunker[2] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.BUNKER_PATHS.get(2)))).getImage();
-
-        // INVADERS
-        invader1[0] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.INVADER1_PATHS.get(0)))).getImage();
-        invader1[1] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.INVADER1_PATHS.get(1)))).getImage();
-
-        invader2[0] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.INVADER2_PATHS.get(0)))).getImage();
-        invader2[1] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.INVADER2_PATHS.get(1)))).getImage();
-
-        invader3[0] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.INVADER3_PATHS.get(0)))).getImage();
-        invader3[1] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.INVADER3_PATHS.get(1)))).getImage();
-
-        invader4[0] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.BONUS_INVADER_PATHS.get(0)))).getImage();
-        invader4[1] = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(UIConstants.BONUS_INVADER_PATHS.get(1)))).getImage();
-    }
-
     @Override
     protected void paintComponent(final Graphics g) {
         super.paintComponent(g);
+
         if (entities == null) {
             throw new IllegalStateException("Null entities in Canvas!");
+        }
+
+        if (spriteManger == null) {
+            initSpriteManger();
         }
 
         final List<EntityView> list = entities;
@@ -196,11 +127,9 @@ public final class Canvas extends JPanel {
             // ENTITIES
             final int x = entity.getPosition().getX() * scaleX;
             final int y = entity.getPosition().getY() * scaleY;
-            final int width = entity.getWidth() * scaleX;
-            final int height = entity.getHeight() * scaleY;
 
             final Image imm = fetchImage(entity);
-            g.drawImage(imm, x, y, width, height, null);
+            g.drawImage(imm, x, y, null);
 
             // BUNKERS LIFE COUNTERS
             if (entity.getType() == EntityType.BUNKER) {
@@ -209,7 +138,6 @@ public final class Canvas extends JPanel {
                 g.drawString(String.valueOf(entity.getHealth()), newX, newY);
             }
         }
-
     }
 
     /**
@@ -221,36 +149,45 @@ public final class Canvas extends JPanel {
 
         switch (entity.getType()) {
             case INVADER_1 -> {
-                return invader1[invadersAnimationFrame.get()];
+                return spriteManger.getImage(entity.getType(), invadersAnimationFrame.get());
             }
             case INVADER_2 -> {
-                return invader2[invadersAnimationFrame.get()];
+                return spriteManger.getImage(entity.getType(), invadersAnimationFrame.get());
             }
             case INVADER_3 -> {
-                return invader3[invadersAnimationFrame.get()];
+                return spriteManger.getImage(entity.getType(), invadersAnimationFrame.get());
             }
             case BONUS_INVADER -> {
-                return invader4[bonusInvaderAnimationFrame.get()];
+                return spriteManger.getImage(entity.getType(), bonusInvaderAnimationFrame.get());
             }
             case PLAYER -> {
-                return player[menuActionsInterface.getChosenShipIndex()];
+                return spriteManger.getImage(entity.getType(), menuActionsInterface.getChosenShipIndex());
             }
             case PLAYER_SHOT -> {
-                return playerShots[menuActionsInterface.getChosenShipIndex()];
+                return spriteManger.getImage(entity.getType(), menuActionsInterface.getChosenShipIndex());
             }
             case INVADER_SHOT -> {
-                return invaderShot;
+                return spriteManger.getImage(entity.getType(), 1);
             }
             case BUNKER -> {
                 if (entity.getHealth() > Constants.BUNKER_HEALTH / 3 * 2) {
-                    return bunker[0];
+                    return spriteManger.getImage(entity.getType(), 0);
                 } else if (entity.getHealth() > Constants.BUNKER_HEALTH / 3) {
-                    return bunker[1];
+                    return spriteManger.getImage(entity.getType(), 1);
                 }
-                return bunker[2];
+                return spriteManger.getImage(entity.getType(), 2);
             }
         }
 
         return voidImage;
+    }
+
+    /**
+     * ...
+     */
+    private void initSpriteManger() {
+        int scaleX = getWidth() / Constants.BORDER_RIGHT;
+        int scaleY = getHeight() / Constants.BORDER_BOTTOM;
+        spriteManger = new SpriteManager(scaleX, scaleY);
     }
 }
