804ac8b37b2e030d0b9ec5f8853699c5d7501f65
Leonardo Lioi <leonardo.lioi@studio.unibo.it>
2026-02-03 08:12:39 +0100

    resetEntities

--- src/main/java/it/unibo/scat/model/game/GameLogic.java
+++ src/main/java/it/unibo/scat/model/game/GameLogic.java
@@ -191,9 +191,7 @@ public class GameLogic {
      * Also removes every shot currently present in the world.
      */
     public void resetEntities() {
-        for (final AbstractEntity e : List.copyOf(gameWorld.getEntities())) {
-            e.reset();
-        }
+
     }
 
     /**
