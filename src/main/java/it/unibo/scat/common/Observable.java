1d673739f567804df3ba87de7e39788122a0c750
Mario Lungu <mario.lungu@studio.unibo.it>
2026-02-13 20:35:51 +0100

    Observable: added javadoc

--- src/main/java/it/unibo/scat/common/Observable.java
+++ src/main/java/it/unibo/scat/common/Observable.java
@@ -1,18 +1,19 @@
 package it.unibo.scat.common;
 
 /**
- * ...
+ * Interface that defines the subject in the Observer pattern.
  */
 public interface Observable {
 
     /**
-     * @param o ...
+     * Sets the observer.
      * 
+     * @param o the instance to bind to this subject.
      */
     void setObserver(Observer o);
 
     /**
-     * ...
+     * Notifies the observer that a state change happened.
      */
     void notifyObserver();
 }
