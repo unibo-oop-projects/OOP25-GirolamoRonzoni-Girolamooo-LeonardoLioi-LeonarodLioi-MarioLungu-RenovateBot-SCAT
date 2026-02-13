3cd264c36f356796bfa1b5e3fc70afdd0f6d3040
Mario Lungu <mario.lungu@studio.unibo.it>
2026-02-13 20:33:20 +0100

    GamePanelInterface: added javadoc

--- src/main/java/it/unibo/scat/view/game/api/GamePanelInterface.java
+++ src/main/java/it/unibo/scat/view/game/api/GamePanelInterface.java
@@ -3,65 +3,61 @@ package it.unibo.scat.view.game.api;
 import it.unibo.scat.common.GameState;
 
 /**
- * ...
+ * Interface for the game panel.
  */
 public interface GamePanelInterface {
     /**
-     * ...
+     * Pauses the game.
      */
     void pause();
 
     /**
-     * ...
+     * Resumes the game from the paused state.
      */
     void resume();
 
     /**
-     * ...
+     * Aborts the current match and returns the user to the main menu.
      */
     void abortGame();
 
     /**
-     * ...
+     * Quits/terminates the application.
      */
     void quit();
 
     /**
-     * @return ...
-     * 
+     * @return the current score to display.
      */
     int getScore();
 
     /**
-     * @return ...
-     * 
+     * @return the current player health (number of hearts).
      */
     int getPlayerHealth();
 
     /**
-     * @return ...
-     * 
+     * @return the current level.
      */
     int getLevel();
 
     /**
-     * @return ...
-     * 
+     * @return the username associated with the current session.
      */
     String getUsername();
 
     /**
-     * ...
+     * Resets the game state to start a new match.
      */
     void restart();
 
     /**
-     * ...
+     * Displays the display of Game Over on the screen.
      */
     void showGameOver();
 
     /**
-     * @return ...
+     * @return the current state of the game.
      */
     GameState getGameState();
 }
