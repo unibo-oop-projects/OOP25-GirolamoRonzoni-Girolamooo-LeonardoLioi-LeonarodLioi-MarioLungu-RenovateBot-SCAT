4ba302ebf9c4a499704f8973db6e118f765ba457
Leonardo Lioi <leonardo.lioi@studio.unibo.it>
2026-02-13 23:32:44 +0100

    Constants: added static field

--- src/main/java/it/unibo/scat/common/Constants.java
+++ src/main/java/it/unibo/scat/common/Constants.java
@@ -7,6 +7,7 @@ public final class Constants {
     // PATHS
     public static final String LEADERBOARD_PATH = "data/leaderboard.txt";
     public static final String ENTITIES_PATH = "data/entities.txt";
+    public static final String RESOURCE_PATH = "/data/leaderboard.txt";
 
     // WORLD
     public static final int BORDER_RIGHT = 60;
