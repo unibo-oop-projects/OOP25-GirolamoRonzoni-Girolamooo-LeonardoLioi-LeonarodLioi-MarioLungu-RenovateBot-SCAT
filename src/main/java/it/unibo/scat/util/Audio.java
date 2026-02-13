50069be646f00d72f9fe848c35041f407651cf28
Leonardo Lioi <leonardo.lioi@studio.unibo.it>
2026-02-13 23:30:15 +0100

    Audio: Javadoc added

--- src/main/java/it/unibo/scat/util/Audio.java
+++ src/main/java/it/unibo/scat/util/Audio.java
@@ -1,29 +1,30 @@
 package it.unibo.scat.util;
 
 /**
- * ...
+ * Interface for managing audio playback.
  * 
  */
 public interface Audio {
 
     /**
-     * ...
+     * Plays the specified track.
      * 
-     * @param audioTrack ...
-     * @param loop       ...
+     * @param audioTrack the track to play.
+     * 
+     * @param loop       to enable continuous looping.
      */
     void play(AudioTrack audioTrack, boolean loop);
 
     /**
-     * ...
+     * Stops the current audio playback.
      * 
      */
     void stop();
 
     /**
-     * ...
+     * Sets the volume of the audio playback.
      * 
-     * @param volume ...
+     * @param volume the volume level to set (between 0.0 and 1.0).
      */
     void setVolume(float volume);
 
